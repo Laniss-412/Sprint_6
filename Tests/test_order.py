43289549c53546262d7b1490f4eae92de7ab7a93
Igor Ashakov <RayarVar@yandex.ru>
2026-04-28 16:46:19 +0300

    Добавлен data.py, исправление BasePage и завершение параметризации

--- Tests/test_order.py
+++ Tests/test_order.py
@@ -2,12 +2,11 @@ import pytest
 import allure
 from Pages.main_page import MainPage
 from Pages.order_page import OrderPage
+from data import OrderData
 
 class TestOrder:
     @allure.title("Проверка оформления заказа")
-    @pytest.mark.parametrize("order_button, name, surname, address, phone, period",
-                             [("top", "Игорь", "Иванов", "ул. Сталина, 10", "77777777777", "сутки"),
-                              ("bottom", "Аркадий", "Андреевич", "ул. Дедина, 13", "77778887788", "двое суток")])
+    @pytest.mark.parametrize("order_button, name, surname, address, phone, period", OrderData.ORDER_DETAILS)
     def test_order_scooter(self, driver, order_button, name, surname, address, phone, period):
         main_page = MainPage(driver)
         order_page = OrderPage(driver)
