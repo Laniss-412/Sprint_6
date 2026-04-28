cbf06bacd439f600f771c2bd976e8f16ed63db84
Igor Ashakov <RayarVar@yandex.ru>
2026-04-28 16:17:57 +0300

    Добавлен файл urls.py

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from urls import Urls
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = "https://qa-scooter.praktikum-services.ru/"
+        self.base_url = Urls.MAIN_PAGE_URL
 
     def open_site(self):
         return self.driver.get(self.base_url)
